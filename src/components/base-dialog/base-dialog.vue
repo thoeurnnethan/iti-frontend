ee1b65f3868144fae49af0eb2203e0316d5ab6d6
nethan <nethanthoeurn19@gmai.com>
2024-06-26 01:19:51 +0700

    Add popup dialog when get error response

---
+++ src/components/base-dialog/base-dialog.vue
@@ -0,0 +1,44 @@
+<template src="./base-dialog.html"></template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { modalController } from '@ionic/vue';
+
+export default defineComponent({
+    name: "baseDialogVue",
+    props: {
+        title: {
+            type: String
+        },
+        content: {
+            type: String
+        },
+        btnLabel: {
+            type: String
+        },
+        showCancel: {
+            type: Boolean,
+            default: true
+        },
+        cancelLabel : {
+            type : String,
+        }
+    },
+    data() {
+        return {}
+    },
+
+    methods: {
+        handleClose() {
+            return modalController.dismiss()
+        },
+        onClickOK() {
+            return modalController.dismiss(null, 'ok')
+        }
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+@import "./base-dialog.scss";
+</style>
