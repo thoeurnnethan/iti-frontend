49bbac08b031682c5465413954b22331d7b5c632
ngoytrylyhuor <lyhourkhe123@gmail.com>
2024-07-09 10:31:43 +0700

    apple orange banana

--- src/views/user/user-register/user-register.vue
+++ src/views/user/user-register/user-register.vue
@@ -1,43 +1,17 @@
 <template src="./user-register.html"></template>
 
 <script lang="ts">
+import { UserRegister } from './UserRegister';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-    /** Option Misc */
-    name: "app",
-    inheritAttrs: false,
     components: {
-        // 
-    },
-
-    data() {
-        return {
-            selectedCity: null,
-            cities: [
-                { name: 'New York', code: 'NY' },
-                { name: 'Rome', code: 'RM' },
-                { name: 'London', code: 'LDN' },
-                { name: 'Istanbul', code: 'IST' },
-                { name: 'Paris', code: 'PRS' }
-            ]
-        };
-    },
-    props: {
-        // 
-    },
-    computed: {
-        // 
-    },
-    methods: {
-        // 
-    },
-    watch: {
-        // 
-    },
-})
+        UserRegister
+    }
+});
 </script>
 
 <style>
 @import url('./user-register.scss');
 </style>
+./UserRegister
