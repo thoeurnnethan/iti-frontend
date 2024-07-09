e08c0b89d52e20ca3c8fb75a9e1058e86e4275bf
ngoytrylyhuor <lyhourkhe123@gmail.com>
2024-07-10 00:35:47 +0700

    qw

--- src/views/user/user-register/user-register.vue
+++ src/views/user/user-register/user-register.vue
@@ -4,11 +4,16 @@
 import { defineComponent } from 'vue';
 import type { FormItem } from '@/shared/types/menu-list';
 import { UserRegister } from './UserRegister';
+import { UserRoleList, GenderCodeList } from '@/shared/common/common';
 
 export default defineComponent({
     data() {
         return {
             formList: [] as FormItem[],
+            userRoleList: UserRoleList,
+            genderCodeList: GenderCodeList,
+            select_Role: '',
+            roleTitle: '',
         }
     },
 
@@ -19,6 +24,10 @@ export default defineComponent({
     methods: {
         getFormList() {
             this.formList = UserRegister.getFormList();
+        },
+
+        selectRole(){
+            this.roleTitle = this.select_Role
         }
     },
 });
