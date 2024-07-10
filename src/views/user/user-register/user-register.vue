6831c5c21793e8a0559dbdb678804945f9bb6b21
ngoytrylyhuor <lyhourkhe123@gmail.com>
2024-07-10 22:31:19 +0700

    teacher-form

--- src/views/user/user-register/user-register.vue
+++ src/views/user/user-register/user-register.vue
@@ -1,19 +1,48 @@
 <template src="./user-register.html"></template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import type { FormItem } from '@/shared/types/menu-list';
+import { USER_LIST, USER_LIST_RES } from '@/shared/types/user-list';
 import { UserRegister } from './UserRegister';
+import { API_PATH } from '@/shared/common/api-path';
+import { RequestService } from '@/shared/services/request-service';
 import { UserRoleList, GenderCodeList } from '@/shared/common/common';
 
 export default defineComponent({
+
+    name: 'UserRegister',
     data() {
+        const dataTable = ref<USER_LIST[]>([]);
         return {
             formList: [] as FormItem[],
             userRoleList: UserRoleList,
             genderCodeList: GenderCodeList,
-            select_Role: '',
-            roleTitle: '',
+            select_Role: '03',
+            dataTable,
+            searchKey: '',
+            totalCount: 0,
+            pageSize: 10,
+            pageNumber: 0,
+            startingIndex: 1,
+            userInfoUpdate: {
+                userID: 123,
+                roleID: '03',
+                firstName: '',
+                lastName: '',
+                nickName: '',
+                gender: '',
+                dateOfBirth: '',
+                phone: '',
+                email: '',
+                passwd: '',
+                imageUrl: '',
+                placeOfBirth: '',
+                address: '',
+                firstLoginDate: '',
+                lastLoginDate: '',
+                statusCode: '',
+            } as USER_LIST,
         }
     },
 
@@ -21,13 +50,41 @@ export default defineComponent({
         this.getFormList();
     },
 
+    computed: {
+        isValidForm(): boolean {
+            return  this.userInfoUpdate.firstName !== '' &&
+                    this.userInfoUpdate.lastName !== '';
+        }
+    },
+
     methods: {
+
+        async teacherSubmit(_item: USER_LIST) {
+            if (!this.isValidForm) {
+                alert('Please fill out all required fields');
+                return;
+            }
+            console.table(this.userInfoUpdate);
+            
+            const reqBody: USER_LIST = this.userInfoUpdate;
+            const response = (await new RequestService().request(API_PATH.USER_REGISTER, reqBody, false)) as USER_LIST_RES;
+            this.$router.push('/user-list');
+            console.log(response);
+        },
+
         getFormList() {
             this.formList = UserRegister.getFormList();
         },
 
         selectRole(){
-            this.roleTitle = this.select_Role
+            this.select_Role
+        },
+
+        handleFileUpload(event: Event) {
+            const target = event.target as HTMLInputElement;
+            const file = target.files ? target.files[0] : null;
+            this.userInfoUpdate.imageUrl = file ? file.name : '';
+            console.log(this.userInfoUpdate.imageUrl);
         }
     },
 });
